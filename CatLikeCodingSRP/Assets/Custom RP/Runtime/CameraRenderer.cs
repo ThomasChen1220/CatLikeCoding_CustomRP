f2478fadca1013b359adec4da06d786420baf55d
ThomasChen <canchen@usc.edu>
2020-12-02 02:36:31 +0800

    first one finished

---
+++ CatLikeCodingSRP/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    CommandBuffer buffer;
+    const string bufferName = "Render Camera";
+
+    CullingResults cullingResults;
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        buffer = new CommandBuffer
+        {
+            name = bufferName
+        };
+
+        PrepareBuffer();
+        PrepareForSceneWindow();
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+    }
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        CameraClearFlags flags = camera.clearFlags;
+        buffer.ClearRenderTarget(
+             flags <= CameraClearFlags.Depth,
+             flags == CameraClearFlags.Color,
+             flags == CameraClearFlags.Color ?
+                camera.backgroundColor.linear : Color.clear
+         );
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
+    }
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+    void DrawVisibleGeometry()
+    {
+        //draw the opaque objects
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(
+            unlitShaderTagId, sortingSettings
+        );
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+        context.DrawRenderers(
+            cullingResults, ref drawingSettings, ref filteringSettings
+        );
+
+        context.DrawSkybox(camera);
+
+        //draw the transparent objects
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+
+        context.DrawRenderers(
+            cullingResults, ref drawingSettings, ref filteringSettings
+        );
+    }
+    void Submit()
+    {
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    
+}
